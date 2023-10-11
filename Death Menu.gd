7179a437eaa096eb37f0286d161fb0318a0d4f89
Miguelito223 <miguelitojimenez650@gmail.com>
2023-10-11 22:18:15 +0200

    e

--- Death Menu.gd
+++ Death Menu.gd
@@ -1,11 +1,11 @@
 extends Control
 
 func _on_return_pressed():
-	PlayerData.load_file()
-	Global.load_scene(self, PlayerData.playerdata.level)
+	Data.load_file()
+	Global.load_scene(self, Data.data.player_data.level)
 
 func _on_back_pressed():
-	PlayerData.load_file()
+	Data.load_file()
 	Global.load_scene(self, "res://main_menu.tscn")
 
 
--- level.gd
+++ level.gd
@@ -7,6 +7,6 @@ func _on_area_2d_body_entered(body):
 
 func _on_victory_zone_body_entered(body):
 	if body.name == "player":
-		PlayerData.playerdata.level = "level_" + str( int(PlayerData.playerdata.level) + 1 ) 
-		PlayerData.save_file()
-		Global.load_scene(self, PlayerData.playerdata.level)
+		Data.data.player_data.level = "level_" + str( int(Data.data.player_data.level) + 1 ) 
+		Data.save_file()
+		Global.load_scene(self, Data.data.player_data.level)
