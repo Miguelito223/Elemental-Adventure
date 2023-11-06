4857f1a6f7634843379cf0b2363aa9e1676ae147
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2023-11-06 17:32:59 +0100

    e

--- Scripts/fireball.gd
+++ Scripts/fireball.gd
@@ -6,12 +6,6 @@ var velocity = Vector2.ZERO
 func _physics_process(delta): 
 	position += velocity * delta 
 		
-
-
-
-	
-
-
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
 
