4696dc7e65ec9c367f583fb68cc865aaa31354e3
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2023-11-06 15:55:46 +0100

    e

--- Scripts/lavaball.gd
+++ Scripts/lavaball.gd
@@ -19,10 +19,12 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 func _on_area_entered(area):
 	if area.get_scene_file_path() == "res://Scenes/enemy.tscn":
 		area.get_parent().damage(10)
+		queue_free()
 
 func _on_body_entered(body):
 	if body.get_scene_file_path() == "res://Scenes/enemy.tscn":
 		body.damage(10)
+		queue_free()
 	else: 
 		queue_free()
 		
