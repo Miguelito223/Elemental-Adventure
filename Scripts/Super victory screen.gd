b4266b8e21acb44ad407a8e58426624310c0a3f3
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2023-11-07 15:49:50 +0100

    e

--- Scripts/Super victory screen.gd
+++ Scripts/Super victory screen.gd
@@ -2,8 +2,8 @@ extends Control
 
 func _process(delta):
 	$Label.text = "You won level: \n level_" + str(int(Globals.level) - 1) + "!"
-	$coins.text = ":" + str(Globals.coins)
-	$score.text = "Score:" + str(Globals.score)
+	$VBoxContainer2/coins.text = ":" + str(Globals.coins)
+	$VBoxContainer2/score.text = "Score:" + str(Globals.score)
 
 
 func _on_back_pressed():
