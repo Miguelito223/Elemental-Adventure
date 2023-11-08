8ad7ec82b05dea3350c061408f43ed67f1c3cc16
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2023-11-08 21:01:38 +0100

    e

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -86,7 +86,7 @@ func flip():
 		speed = abs(speed) * -1
 
 func simelball():
-	await get_tree().create_timer(3)
+	await get_tree().create_timer(5)
 	var slime_ball = slimeballs.instantiate()
 	slime_ball.get_node("PointLight2D").emitting = false
 	slime_ball.modulate = Color("green")
