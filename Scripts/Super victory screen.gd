6b7e85428f9e612c3644d6ae8c2677e3d8646454
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2023-11-13 16:15:30 +0100

    e

--- Scripts/Super victory screen.gd
+++ Scripts/Super victory screen.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b37827673d6c11f9d51d84cb706947ab6540cdf919ef1e2d23b368f45e963b8
-size 377
+oid sha256:ea1ce587641d7e837f0ed23bb007ffacb940ccb4f19546286ef93d4daa385dc7
+size 379
