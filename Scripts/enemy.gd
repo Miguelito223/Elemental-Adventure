872a4e93c1f55643523803202e220f5fd95caaae
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2023-11-19 16:40:13 +0100

    e

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d302b962599bde7d8db58d0229d130a6bbe45c044fde6d5a84a1ce6d20378520
-size 5016
+oid sha256:5592c3cb403c07aa02064e24cb888f5a30a18c19321efc4a8d504e34e56c3f9e
+size 4985
--- Scripts/enemy_boss.gd
+++ Scripts/enemy_boss.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94333353f5dcc80c2bcb4c0d77dd327fc880f6b9fee8253794e3d67f5caa0ed3
-size 3820
+oid sha256:01b5c8a1eb6135434f82346dab04dc02f96017f089b7aadc8bbc49bfc53862e0
+size 3788
