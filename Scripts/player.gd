3aed30a474f7279d036ca6f344751b6e7eaa0bf9
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2023-11-23 20:25:22 +0100

    e

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c20a672bd795d55dd47b0976fd4b348878e97fd01f107c612bac2325b0301a6
-size 9215
+oid sha256:afc8d6d13b0f44cfdf223460034d9a8a7b379b75819dff6a9b006f5dade6423b
+size 9216
