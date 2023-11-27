ea085360e269f3699f1fdca9e08f077d6ec9220c
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2023-11-27 17:43:02 +0100

    e

--- Scripts/Data.gd
+++ Scripts/Data.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a05e3a517b681a67d71dc9d867189fb83c775ec37e886a84e812bda20f7a8e51
-size 3870
+oid sha256:99d04ca6e04aa8e49368b7e390f340002cffa073df56f06a29efdc62a899dae5
+size 3858
--- Scripts/DataState.gd
+++ Scripts/DataState.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9632f5b6abbe77044dfcf307fc15446f2635181f2d311a55ebe5f643b9e9e69
-size 3404
+oid sha256:84aab282b7fd0cf34644baaa204bb89eff4422dbec61226bde10d86928aa23fc
+size 3400
