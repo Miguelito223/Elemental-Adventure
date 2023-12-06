b1f796deac68aef3de12d6f90c0f416ae75e4045
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2023-12-06 19:33:44 +0100

    E

--- Scripts/Globals.gd
+++ Scripts/Globals.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8f4af4aaa76f747d1275205e2b671fae9e05fb6daf5c076e9e3b45665a0e6b0
-size 1213
+oid sha256:80cd8cc433c4c3ac08f486f9a8ea1d44726b035c5033f472ccfb4626ee794369
+size 1149
