8e2893f967f4c574241039c69fe0289c0fd3eeca
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2023-12-06 19:09:39 +0100

    e

--- Scripts/Victory menu.gd
+++ Scripts/Victory menu.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38e1085c2f465cc63f65e7aae66810d6fe7de9a020dc6b215accdab26f8c87d8
-size 461
+oid sha256:5b241b6bec5e5b070511cae8170dbf43083f8803c77864d639cf634f90e460d3
+size 466
--- Scripts/level.gd
+++ Scripts/level.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af68336c25a43e5c44464c93ea1c1359f25d3da92542193646d69871a0bdf3a4
+oid sha256:20851c4e965e3da76b8ff6c02b696753733dd39bedd447ee6403e7a59d3aa6a4
 size 11465
