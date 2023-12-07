dd2ce038af33ec0fa0db069d2f09bc454e935ed8
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2023-12-07 20:58:40 +0100

    eh

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b4e473f0b7f6552e1f2447065045397221842c9225d165fcbc6ef319451bb8e
-size 4927
+oid sha256:933d2899d55ad784ae8a733865b5efdea82ecbadb1a95442a028d2f88ef15dc2
+size 5070
