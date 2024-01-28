5489e3267d93f00f60b3c5a7957088aee22584e6
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-01-28 09:53:32 +0100

    xd

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e27c371ad107c29bb2bac32215132ee2a49ff6d9690b975431db6dcefc5547a
-size 5729
+oid sha256:7e341157d03c382184c21e60bf6948fc684d76fc759bbbb696f06d8c138f4b14
+size 5935
