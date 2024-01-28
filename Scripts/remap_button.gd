69166a1ddc3b8d509b80de36cf0e0266cbf2b841
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-01-28 13:23:34 +0100

    e

--- Scripts/remap_button.gd
+++ Scripts/remap_button.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6b93f782a22ecd3b471133657ae3af92364dd1e25c5fe787896d3732fa526c3
-size 722
+oid sha256:33a097ddddce845c68acf8f0fadd339ae88b7a3d990b977c0e38dd1852cd04a9
+size 717
