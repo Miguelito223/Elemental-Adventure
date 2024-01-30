4ed31c5bf8e89c85bf9aee7d7070126e12fea790
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-01-30 16:49:36 +0100

    eh

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3553f0161c735045397eb55add7eafbede5931473e60f8465bc61ba6732984aa
-size 3744
+oid sha256:c0c1ef147648e4df753a9d07a443544ebbdd8e8c62059f6d5958c37561cc8f68
+size 3796
