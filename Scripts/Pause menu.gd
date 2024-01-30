9e9d34a03e049f280477296f32afff7852a620b2
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-01-30 16:56:30 +0100

    e

--- Scripts/Pause menu.gd
+++ Scripts/Pause menu.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ba262d7158ad3b9969b56793e881871f361a128a02dbfdf728f50654c3c16e1
-size 8788
+oid sha256:e5ecccda6017f7c8a4436d3b67d49097690a6a80f36d3c21ba29a46cc9e80499
+size 8796
--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4df55320c8e24c0aa4e6084dd191d7866bf0eae6d0cbfdbc1228adae94ae6756
-size 8529
+oid sha256:b8daf71c25049d1cd625e60cc29ea146f5aabd288071446c3a7e54f96b47551f
+size 8537
