fc7862a8082c1e1ff4f226cfb21bc6a5a8ed3f78
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-02-02 14:26:30 +0100

    e

--- Scripts/energy.gd
+++ Scripts/energy.gd
@@ -1,35 +1,3 @@
-extends RigidBody2D
-
-var rng = RandomNumberGenerator.new()
-
-func _ready():
-	rng.randomize()
-
-func _on_area_2d_body_entered(body):
-	if body.is_in_group("player"):
-			body.getenergy()
-			visible = false
-			$"coin sound".play()
-			await $"coin sound".finished
-			queue_free()
-
-func save_state():
-	return {
-		"filename" : get_scene_file_path(),
-		"parent" : get_parent().get_path(),
-		"name" : name,
-		"size_x" : scale.x,
-		"size_y" : scale.y,
-		"pos_x" : position.x, # Vector2 is not supported by JSON
-		"pos_y" : position.y,
-		"visible": visible,
-	}
-
-func load_state(info):
-	name = info.name
-	position = Vector2(info.pos_x, info.pos_y)
-	scale = Vector2(info.size_x, info.size_y)
-	visible = info.visible
-
-
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:889ad8612c6959a2dd39ac1529a786f8c27e4a5def65b471c88274b92142a71b
+size 792
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9393f321d1ba0f8b6c5ed1ca8cec16583784eb424a8754b8499530c75e1ac4a3
+oid sha256:e059682685b515f27f2d9d0da585837d8914f96e40e44d795cdf5f89adb50a36
 size 9881
