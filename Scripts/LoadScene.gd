db640977109fc331e28b40aa4b5a49c13a4c5833
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-02-02 22:13:17 +0100

    1

--- Scripts/LoadScene.gd
+++ Scripts/LoadScene.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7ee61d277282ed184b08a0f9c972ba99ab59e10b6e24027a90bfbfcb2281098
-size 2365
+oid sha256:05e3816266524d45d0f4c804e1865c0c49350cd2fb398e46e310b5e0e3744630
+size 2451
