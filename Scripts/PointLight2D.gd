c3ac1ad1a53e90a3eea341325c00e8d8923cb434
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-02-07 20:45:56 +0100

    eh

---
+++ Scripts/PointLight2D.gd
@@ -0,0 +1,13 @@
+extends PointLight2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	enabled = Globals.Graphics
+	shadow_enabled = Globals.Graphics
+	shadow_filter = Globals.Graphics
