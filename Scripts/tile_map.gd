30dbec8b1fc0babf1d587ce53aa73ab53f9525c4
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-02-11 16:43:00 +0100

    aaaa

--- Scripts/tile_map.gd
+++ Scripts/tile_map.gd
@@ -1,25 +1,36 @@
 extends TileMap
 
-enum {
+@export var noise_imagen: NoiseTexture2D
+@export var cave_noise_imagen: NoiseTexture2D
 
-	GRASS = 0,
-	DIRT = 1,
-	ROCK = 4,
-	SAND = 5,
-}
+var width = 10000
+var height = 100
 
-var noise = $noise.texture.noise
+var dirt_grass_atlas = Vector2i(0,0)
+var dirt_atlas = Vector2i(1,0)
 
+var tile_arg: Array = []
 
 func _ready():
-	randomize()
-	noise.seed = randi() % 1000
+	var noise: FastNoiseLite = noise_imagen.noise
+	var noise_height 
+	var cave_noise: FastNoiseLite = cave_noise_imagen.noise
+	for x in width:
+		noise_height = int(noise.get_noise_1d(x) * 10)
 
-	for x in 100:
-		var ground = abs(noise.get_noise_2d(x,0) * 60)
-		for y in range(ground, 100):
-			if noise.get_noise_2d(x, y) > 0: 
-				set_cell(0 , Vector2i(x, y), DIRT)
-				if randi() % 2 ==  1 and get_cell_source_id(0, Vector2i(x,y-1)) == -1:
-					set_cell(0, Vector2i(x,y-1), GRASS)
+		for y in height:
+			if y > 5:
+				set_cell(1, Vector2i(x, noise_height + y),0, dirt_atlas)
+
+
+			if cave_noise.get_noise_2d(x,y) < 0.4:
+				tile_arg.append(Vector2i(x, noise_height + y))
+
+
+		if tile_arg.find(Vector2i(x, noise_height + 1)) != 1:
+			tile_arg.append(Vector2i(x, noise_height))
+				
+
+	BetterTerrain.set_cells(self, 0, tile_arg, 0)
+	BetterTerrain.update_terrain_cells(self, 0, tile_arg, true)
 
