ae88d5554091665d99881f60ba41377043e04c23
Miguelito223xd <you@example.com>
2024-02-12 13:40:32 +0100

    eh

--- Scripts/tile_map.gd
+++ Scripts/tile_map.gd
@@ -2,19 +2,27 @@ extends TileMap
 
 @export var noise_imagen: NoiseTexture2D
 @export var cave_noise_imagen: NoiseTexture2D
+@export var rock_noise_imagen: NoiseTexture2D
 
 var width = 1000
 var height = 100
 
 var dirt_grass_atlas = Vector2i(0,0)
 var dirt_atlas = Vector2i(1,0)
+var rock_atlas = Vector2i(4,0)
 
 var tile_arg: Array = []
 
-@rpc("any_peer", "call_local", "reliable")
+@rpc("call_local")
 func terrain_generation():
 	var noise: FastNoiseLite = noise_imagen.noise
 	var cave_noise: FastNoiseLite = cave_noise_imagen.noise
+	var rock_noise: FastNoiseLite = rock_noise_imagen.noise
+	
+	cave_noise.seed = randi()
+	noise.seed = randi()
+	rock_noise.seed = randi()
+
 	var noise_height 
 
 	for x in width:
@@ -26,7 +34,14 @@ func terrain_generation():
 
 
 			if cave_noise.get_noise_2d(x,y) < 0.4:
-				tile_arg.append(Vector2i(x, noise_height + y))
+				if rock_noise.get_noise_2d(x,y) < -0.4:
+					set_cell(0, Vector2i(x, noise_height + y),0, rock_atlas)
+				else:
+					tile_arg.append(Vector2i(x, noise_height + y))
+				
+
+				
+
 
 
 		if tile_arg.find(Vector2i(x, noise_height + 1)) != 1:
@@ -37,5 +52,8 @@ func terrain_generation():
 	BetterTerrain.update_terrain_cells(self, 0, tile_arg, true)
 
 func _ready():
+	if not Network.is_networking:
+		return
+	
 	terrain_generation.rpc()
 
