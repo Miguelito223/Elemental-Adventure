5578be7876c547f1346fa0500f8d0f4d06f7365c
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-02-13 18:09:17 +0100

    fixed bug

--- Scripts/game over Menu.gd
+++ Scripts/game over Menu.gd
@@ -1,13 +1,16 @@
 extends Control
 
+@onready var player = get_parent().get_node(Globals.map).get_node(str(get_tree().get_multiplayer().get_unique_id()))
+
 func _ready():
 	if not Network.is_networking:
 		get_tree().paused = true
 
 func _on_return_pressed():
 	if Network.is_networking:
-		print("removing game over menu...")
-		self.queue_free()
+		if player.is_multiplayer_authority():
+			print("removing game over menu...")
+			self.queue_free()
 	else:
 		get_tree().paused = false
 		LoadScene.load_scene(self, Globals.level)
@@ -15,7 +18,7 @@ func _on_return_pressed():
 
 func _on_back_pressed():
 	if Network.is_networking:
-		if is_multiplayer_authority():
+		if player.is_multiplayer_authority():
 			get_parent().multiplayer.multiplayer_peer = null
 			Network.is_networking = false
 			Network.connected_ids.clear()
