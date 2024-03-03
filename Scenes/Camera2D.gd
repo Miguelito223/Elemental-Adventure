cde1d727f3a8b8570cee27ec6bd2e324057fb489
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-03-03 15:20:12 +0100

    ups

--- Scenes/Camera2D.gd
+++ Scenes/Camera2D.gd
@@ -2,6 +2,6 @@ extends Camera2D
 
 func _process(_delta):
 	if Network.is_networking:
-		$Environment.visible = true
+		$environment.visible = true
 	else: 
-		$Environment.visible = false
+		$environment.visible = false
