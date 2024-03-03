5383a87802e0d7de4f559aea07b47e5783902bfb
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-03-03 15:51:35 +0100

    eh

--- Scenes/Sun.gd
+++ Scenes/Sun.gd
@@ -3,4 +3,4 @@ extends Node2D
 func _process(_delta):
 	if Network.is_networking:
 		self.visible = is_multiplayer_authority()
-	self.rotation_degrees = (Globals.hour * 60) * 0.0166667
+	self.rotation_degrees = (Globals.hour * 60) / 60
