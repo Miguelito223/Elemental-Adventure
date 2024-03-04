6e514c5996c5b3a6bf74dbafb30d3329b75e18e0
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-03-04 16:05:46 +0100

    xd

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -95,7 +95,7 @@ func _enter_tree():
 
 func _input(event):
 	if Network.is_networking:
-		if is_multiplayer_authority():
+		if syncronizer.is_multiplayer_authority():
 			if event.is_action_pressed(ui_inputs.keys()[5]):
 				position.y += 1
 			
