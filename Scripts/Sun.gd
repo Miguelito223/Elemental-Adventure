17961259aabd0d1daede3f2b04bed0e4589a779b
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2024-03-04 16:24:02 +0100

    fixed

--- Scripts/Sun.gd
+++ Scripts/Sun.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 func _process(_delta):
 	if Network.is_networking:
-		self.visible = is_multiplayer_authority()
+		self.visible = get_parent().get_parent().get_parent().get_parent().get_parent().is_multiplayer_authority()
 		self.rotation_degrees = (Globals.hour * 60) / 60
 	else:
 		self.visible = true
